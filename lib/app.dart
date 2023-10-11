9027cadd380dc5a619279d738dee633dcd88284e
Bruno Minelli <obrunominelli@gmail.com>
2023-10-11 16:42:49 -0300

    style: Inclusão de paleta de cores do projeto

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/views/home_view.dart';
+import 'package:portfolio/routes/app_routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,16 +10,18 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.transparent,
-            primary: Colors.transparent,
-            secondary: Colors.transparent,
-            tertiary: Colors.transparent,
-            background: Colors.transparent,
-            surface: Colors.transparent,
-          ),
-          textTheme: TextTheme()),
-      home: const HomeView(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(88, 68, 100, 1),
+          primary: const Color.fromRGBO(88, 68, 100, 1),
+          secondary: const Color.fromRGBO(52, 38, 56, 1),
+          tertiary: const Color.fromRGBO(245, 240, 250, 1),
+          background: const Color.fromRGBO(254, 251, 251, 1),
+          surface: const Color.fromRGBO(20, 20, 20, 1),
+        ),
+        textTheme: GoogleFonts.robotoTextTheme(),
+      ),
+      routes: routes,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
