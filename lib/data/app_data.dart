a734be3d81788703786f71a2ee1ebc3b3c6becc8
Bruno Minelli <obrunominelli@gmail.com>
2023-10-20 12:36:40 -0300

    refactor: Nova stack do portfolio

--- lib/data/app_data.dart
+++
@@ -1,6 +0,0 @@
-class AppData {
-  static Map<String, dynamic> navigationButtons = {};
-  static Map<String, dynamic> aboutMe = {};
-  static Map<String, dynamic> services = {};
-  static Map<String, dynamic> experience = {};
-}
