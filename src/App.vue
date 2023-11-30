4a9b15f370540de481e98e93b9f8c603ad54a02d
Bruno Minelli <obrunominelli@gmail.com>
2023-11-30 10:27:45 -0300

    chore: Ajuste de arquivos para deploy

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HomePage from "./pages/home/HomePage.vue";
-import FooterComponent from "./components/FooterComponent.vue";
-</script>
-
-<template>
-  <HomePage />
-  <FooterComponent />
-</template>
