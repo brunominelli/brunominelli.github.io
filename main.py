ffb171953c9e1931d6327fc670bc60589c449ae9
Bruno Minelli <brunominelli.dev@gmail.com>
2026-06-09 11:20:17 -0300

    fix: Correção para o git pages

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from app import App
+from flask_frozen import Freezer
 
 app = App().create_app()
+app.config['TESTING'] = True 
+app.config['PROPAGATE_EXCEPTIONS'] = True
+freezer = Freezer(app=app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    freezer.freeze()
+    # app.run(debug=True)
